995b2fd0174d4c2181343ea12427f406caf46028
Jose <josemrozas@gmail.com>
2019-02-20 11:06:03 +0100

    First commits

---
+++ js_etiquetas_con_imagenes/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+{
+  'name':'JS Etiquetas con imagen',
+  'summary': 'Imprime las etiquetas de los productos con su imagen',
+  'description': """Este módulo cambia el comportamiento de la opción de la lista Imprimir > Etiquetas de productos.
+                Muestra la imagen y elimina el código de barras.
+                La referencia interna y el nombre del producto se muestran en líneas distintas""",
+  'version':'1.0',
+  'author':'Jim Sports',
+  'data': [
+    'views/layout.xml'
+  ],
+  'category': 'Advanced Reporting',
+  'depends': [],
+}
+
---
+++ js_modo_catalogo/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+
+{
+  'name':'JS Modo Catálogo',
+  'summary': 'Modifica las vistas de la tienda para eliminar precios y botón de carrito. Para el Tema Clarico',
+  'description': '',
+  'version':'1.0',
+  'author':'Jim Sports',
+  'data': [
+    'views/layout.xml'
+  ],
+  'category': 'eCommerce',
+  'depends': ['base', 'website', 'sale', 'product', 'web'],
+}
