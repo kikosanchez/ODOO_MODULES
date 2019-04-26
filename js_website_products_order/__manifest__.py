e6fec1af79f5dc3b388635571bdcd7fbc5f9eb7b
Pablo <31565658+pablo-lp@users.noreply.github.com>
2019-04-26 15:00:24 +0200

    Módulos Dominicana

--- js_website_products_order/__manifest__.py
+++ js_website_products_order/__manifest__.py
@@ -1,12 +1,12 @@
 {
     "name": "JS Website Products Order",
-    "summary": "Permite seleccionar los productos que aparecen de primeros en el eCommerce y su orden (solo para tema Clarico)",
+    "summary": "Permite seleccionar los productos que aparecen de primeros en el eCommerce y su orden",
     "version": "11.0.1.0",
     "license": "AGPL-3",
     "author": "Jim Sports",
     "category": "Website",
     "website": "https://jimsports.com",
-    "depends": ["website", "website_sale", "clarico_shop"],
+    "depends": ["website", "website_sale"],
     "data": [
         "views/products.xml"
     ],
