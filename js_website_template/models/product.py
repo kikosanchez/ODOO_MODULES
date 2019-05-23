f18f36ed063fb8ed1e03f11d357e6937e10be92e
Pablo <31565658+pablo-lp@users.noreply.github.com>
2019-05-23 11:16:22 +0200

    Últimas correcciones

--- js_website_template/models/product.py
+++ js_website_template/models/product.py
@@ -11,20 +11,36 @@ class ProductTemplate(models.Model):
         super(ProductTemplate, self)._website_price()
 
         for template in self:
-            # Para guardar los precios
+            # Lista para guardar los precios
             product_prices = []
 
             # Precio de la plantilla
             if (template.sale_ok and template.website_price > 0):
-                product_prices.append(template.website_price)
+                # Aquí se usa sudo para evitar discrepancias en el precio
+                # entre un usuario logueado y anónimo (no debería hacer falta)
+                product_prices.append(template.sudo().website_price)
 
             # Precio de las variantes
-            # Se necesita usar sudo() para el visitante anónimo
+            # - se necesita usar sudo() para el visitante anónimo -
             for p in template.sudo().product_variant_ids:
-                # Obtener el precio de cada variante si es mayor que 0, tiene stock y se puede comprar
-                if (p.type == 'product' and p.sale_ok and p.website_price > 0 and (p.inventory_availability == "never" or p.inventory_availability == "custom" or p.qty_available > 0)) or ((p.type == 'consu' or p.type == 'service') and p.sale_ok and p.website_price > 0):
+                # Se puede vender, tiene precio y ...
+                if (p.sale_ok and p.website_price > 0 and (
+                        # Es un producto almacenable y ...
+                        p.type == 'product' and (
+                            # Se vende si hay stock
+                            (p.inventory_availability == "always" and p.qty_available > 0)
+                            # Se vende si el stock está por encima de un umbral
+                            or (p.inventory_availability == "threshold" and p.qty_available > p.available_threshold)
+                            # Se vende independientemente del stock
+                            or (p.inventory_availability == "never" or p.inventory_availability == "custom")
+                        )
+                    ) or (
+                        # No es un producto almacenable
+                        (p.type == 'consu' or p.type == 'service')
+                    )):
+                    # Guardar en la lista el precio de la variante
                     product_prices.append(p.website_price)
 
-            # Obtenemos el precio más bajo y el más alto
+            # Obtenemos el precio más bajo y el más alto de la lista
             template.website_price_min = min(product_prices) if len(product_prices) else 0.00
             template.website_price_max = max(product_prices) if len(product_prices) else 0.00
