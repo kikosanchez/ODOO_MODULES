18a20bcd95caf98dd6f5217ed9c3cbc9512c1abb
Pablo <31565658+pablo-lp@users.noreply.github.com>
2019-08-30 10:58:16 +0200

    Correcciones

--- js_website_template/controllers/web.py
+++ js_website_template/controllers/web.py
@@ -7,6 +7,6 @@ class JsBinary(Binary):
     # Para cambiar la imágen por defecto cuando no hay una
     def placeholder(self, image='placeholder.png'):
         if image == 'placeholder.png':
-            addons_path = http.addons_manifest['web']['addons_path']
-            return open(os.path.join(addons_path, 'js_website_template', 'static', 'img', image), 'rb').read()
+            js_website_template_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+            return open(os.path.join(js_website_template_path, 'static', 'img', image), 'rb').read()
         return super(JsBinary, self).placeholder(image)
