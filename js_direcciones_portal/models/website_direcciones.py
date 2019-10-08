c2225aef4547bfc2cdc3ab5731b8c14a8932e6a8
Jose <josemrozas@gmail.com>
2019-10-08 11:20:05 +0200

    Añadir módulo js_direcciones_portal

---
+++ js_direcciones_portal/models/website_direcciones.py
@@ -0,0 +1,38 @@
+from odoo import api, fields, models, tools
+from odoo.http import request
+
+class DirecccionesWebsite(models.Model):
+    _inherit = 'website'
+
+    ##############################################################################################################    
+    # Esta función sólo se ejecuta cuando NO hay un pedido activo (aunque sea un presupuesto vacío, sin artículos)
+    # En las pruebas tenía que ir a borrarlo en el backend cada vez
+    @api.multi
+    def _prepare_sale_order_values(self, partner, pricelist):
+        self.ensure_one()
+        affiliate_id = request.session.get('affiliate_id')
+        salesperson_id = affiliate_id if self.env['res.users'].sudo().browse(affiliate_id).exists() else request.website.salesperson_id.id
+        addr = partner.address_get(['delivery', 'invoice'])
+        default_user_id = partner.parent_id.user_id.id or partner.user_id.id
+        values = {
+            'partner_id': partner.id,
+            'pricelist_id': pricelist.id,
+            'payment_term_id': self.sale_get_payment_term(partner),
+            'team_id': self.salesteam_id.id or partner.parent_id.team_id.id or partner.team_id.id,
+            'partner_invoice_id': addr['invoice'],
+            'partner_shipping_id': addr['delivery'],
+            'user_id': salesperson_id or self.salesperson_id.id or default_user_id,
+        }
+
+        ############################################################################################################
+        # comprobar si el padre tiene algún hijo que sea dirección de facturación y asignarlo a 'partner_invoice_id'
+        if partner.child_ids:
+            for hijo in partner.child_ids:
+                if hijo.type == 'invoice':
+                    values['partner_invoice_id'] = hijo.id
+        
+        company = self.company_id or pricelist.company_id
+        if company:
+            values['company_id'] = company.id
+
+        return values
---
+++ js_direcciones_portal/wizard/direcciones.py
@@ -0,0 +1,35 @@
+# import logging
+# from odoo.tools.translate import _
+# from odoo.tools import email_split
+# from odoo.exceptions import UserError
+# from odoo import api, models, fields
+# _logger = logging.getLogger(__name__)
+
+from odoo import models, api
+from odoo.addons.portal.wizard.portal_wizard import PortalWizard
+
+# Override /addons/portal/wizard/portal_wizard.py > onchange_portal_id
+class JSPortalWizard(PortalWizard):
+    # Hacer que sólo aparezca el contacto padre en el listado de Acceso al Portal
+    @api.onchange('portal_id')
+    def onchange_portal_id(self):        
+        # for each partner, determine corresponding portal.wizard.user records
+        partner_ids = self.env.context.get('active_ids', [])
+        contact_ids = set()
+        user_changes = []
+        for partner in self.env['res.partner'].sudo().browse(partner_ids):            
+            # Aquí se decide cuales son los partners que aparecen en el listado.
+            contact_partners = [partner] # así estaba antes: contact_partners = partner.child_ids or [partner]
+            for contact in contact_partners:
+                # make sure that each contact appears at most once in the list
+                if contact.id not in contact_ids:
+                    contact_ids.add(contact.id)
+                    in_portal = False
+                    if contact.user_ids:
+                        in_portal = self.portal_id in contact.user_ids[0].groups_id
+                    user_changes.append((0, 0, {
+                        'partner_id': contact.id,
+                        'email': contact.email,
+                        'in_portal': in_portal,
+                    }))
+        self.user_ids = user_changes
